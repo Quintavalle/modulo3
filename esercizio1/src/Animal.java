eb9fbce29e801985c8b625bca2cebcfd14539f62
Quintavalle <147600523+Quintavalle@users.noreply.github.com>
2023-10-26 17:10:32 +0200

    aggiunta classe Animal e classe Cat

---
+++ esercizio1/src/Animal.java
@@ -0,0 +1,6 @@
+public class Animal {
+
+    public void faiIlVerso(){
+        System.out.println("fail il verso");
+    }
+}
---
+++ esercizio1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        Cat cat = new Cat();
+
+        animal1.faiIlVerso();
+        cat.faiIlVerso();
+
+
+    }
+}
